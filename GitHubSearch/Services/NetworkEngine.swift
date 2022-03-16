f824847934ec69b18550a23acb487578e538a764
Javid P <17307316+jap99@users.noreply.github.com>
2022-03-16 06:05:19 -0700

    Update NetworkEngine.swift

--- GitHubSearch/Services/NetworkEngine.swift
+++ GitHubSearch/Services/NetworkEngine.swift
@@ -13,6 +13,8 @@ enum Result {
     case error(Error)
 }
 
+
+
 enum Endpoint {
     case getUsers
     case getUserDetails
@@ -34,26 +36,21 @@ enum Endpoint {
 }
 
 
-// currently calls getRepoCount() after ALL search results are mapped to User objects
-// currently sends array of Users to getRepoCount().
-// concerned w/ whether it's better to call getRepoCount() individually for each User instead of sending it all the users at once
-
-
-// concerned about how to link the count to the correct user
-// concerned w/ how to do it efficiently
-// conerned w/ whether I am creating new instances of each User object when I send them back to searchForUsers(_:_:)
-// concerned w/ whether there's a better way
-// concerned w/ how this will work when user starts typing & too many network requests are made & old ones need to be canceled
-
 
 class NetworkEngine {
     
     private let engine: NetworkEngineProtocol
     
+ 
+    // MARK: - INIT
+ 
     init(engine: NetworkEngineProtocol = URLSession.shared) {
         self.engine = engine
     }
     
+ 
+     // MARK: - ACTIONS
+ 
     func searchForUsers(for text: String, completionHandler: @escaping ([User]?) -> Void) {
         let url = URL(string: Endpoint.getUsers.url.replacingOccurrences(of: "[searchTerm]", with: text))!
         var usersArray: [User] = []
@@ -75,10 +72,11 @@ class NetworkEngine {
                     completionHandler(usersArray)
                 }
             } else if let _ = error {
-                completionHandler(nil)                              // Does not handle error elegantly
+                completionHandler(nil)                             
             }
         }
     }
+ 
     
     func getRepoCount(_ user: User, completionHandler: @escaping (User?) -> Void) {
         var user = user
@@ -93,41 +91,12 @@ class NetworkEngine {
                     completionHandler(user)
                 }
             } else if error != nil  {
-                completionHandler(nil)                               // Returns 0 if error is thrown
+                completionHandler(nil)           
             }
         }
     }
-    
-//    func getRepoCount(_ users: [User], completionHandler: @escaping ([User]?) -> Void) {
-//        var usersArray = [User]()
-//        let s = DispatchSemaphore(value: 0)
-//        for user in users {
-//            var user = user
-//            let url = URL(string: Endpoint.getRepositories.url.replacingOccurrences(of: "[searchTerm]", with: user.username))!
-//            self.engine.performRequest(for: url) { (data, response, error) in
-//                s.signal()
-//                if let data = data,
-//                    let response = response as? HTTPURLResponse,
-//                    response.statusCode == 200 {
-//                    if let jsonData = try? JSONSerialization.jsonObject(with: data, options: .allowFragments) as? [String: Any],
-//                        let count = jsonData["total_count"] as? Int {
-//                        // concerned about how to link the count to the correct user
-//                        // concerned w/ how to do it efficiently
-//                        // conerned w/ whether I am creating new instances of each User object when I send them back to searchForUsers(_:_:)
-//                        // concerned w/ whether there's a better way
-//                        // concerned w/ how this will work when user starts typing & too many network requests are made & old ones need to be canceled
-//                        user.repoCount = count
-//                        usersArray.append(user)
-//                    }
-//                } else if error != nil  {
-//                    completionHandler(nil)                    // Returns 0 if error is thrown
-//                }
-//            }
-//        }
-//        s.wait()
-//        completionHandler(usersArray)
-//    }
-    
+
+ 
     func getUserDetails(_ keywordSearchText: String, completionHandler: @escaping (User?) -> Void) {
         let url: URL = URL(string: Endpoint.getUserDetails.url.replacingOccurrences(of: "[searchTerm]", with: keywordSearchText))!
         engine.performRequest(for: url) { (data, response, error) in
@@ -150,10 +119,12 @@ class NetworkEngine {
         }
     }
     
+ 
     func getRepositoryCount(_ keywordSearchText: String, completionHandler: @escaping (Int?) -> Void) {
 
     }
     
+ 
     func getRepositories(_ keywordSearchText: String, completionHandler: @escaping ([User]?) -> Void) {
         let url: URL = URL(string: Endpoint.getRepositories.url.replacingOccurrences(of: "[searchTerm]", with: keywordSearchText))!
         engine.performRequest(for: url) { (data, response, error) in
@@ -176,15 +147,38 @@ class NetworkEngine {
     }
     
 
-    //    private var userSearchWorkItem: DispatchWorkItem?
-    //    private let queue = DispatchQueue(label: "Concurrent Queue", attributes: .concurrent)
-    //    private let concurrentTasks: Int = 10
+     
+//    func getRepoCount(_ users: [User], completionHandler: @escaping ([User]?) -> Void) {
+//        var usersArray = [User]()
+//        let s = DispatchSemaphore(value: 0)
+//        for user in users {
+//            var user = user
+//            let url = URL(string: Endpoint.getRepositories.url.replacingOccurrences(of: "[searchTerm]", with: user.username))!
+//            self.engine.performRequest(for: url) { (data, response, error) in
+//                s.signal()
+//                if let data = data,
+//                    let response = response as? HTTPURLResponse,
+//                    response.statusCode == 200 {
+//                    if let jsonData = try? JSONSerialization.jsonObject(with: data, options: .allowFragments) as? [String: Any],
+//                        let count = jsonData["total_count"] as? Int {
+//                        user.repoCount = count
+//                        usersArray.append(user)
+//                    }
+//                } else if error != nil  {
+//                    completionHandler(nil)                
+//                }
+//            }
+//        }
+//        s.wait()
+//        completionHandler(usersArray)
+//    }
     
- //        let url: URL = URL(string: Endpoint.getStarCount.url.replacingOccurrences(of: "[searchTerm]", with: keywordSearchText))!
+ 
+//    private var userSearchWorkItem: DispatchWorkItem?
+//    private let queue = DispatchQueue(label: "Concurrent Queue", attributes: .concurrent)
+//    private let concurrentTasks: Int = 10
     
-}
-
-
+//        let url: URL = URL(string: Endpoint.getStarCount.url.replacingOccurrences(of: "[searchTerm]", with: keywordSearchText))!
 //                guard let fileURL = data else { return }
 //                do {
 //                    let documentsURL = try FileManager.default.url(for: .documentDirectory, in: .userDomainMask, appropriateFor: nil, create: false)
