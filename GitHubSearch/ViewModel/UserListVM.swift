e946049048b324318c7bb7cfb196edf3101d0401
Javid P <17307316+jap99@users.noreply.github.com>
2022-03-16 06:09:15 -0700

    Update UserListVM.swift

--- GitHubSearch/ViewModel/UserListVM.swift
+++ GitHubSearch/ViewModel/UserListVM.swift
@@ -4,67 +4,46 @@
 //
 //  Created by Javid Poornasir on 4/18/20.
 //  Copyright © 2020 Javid Poornasir. All rights reserved.
-//
+
 
 import Foundation
 import UIKit
  
-//struct WeatherViewModel: Decodable {
-//
-//    let name: Dynamic<String>
-//    var currentTemperature: TemperatureViewModel
-//
-//    init(from decoder: Decoder) throws {
-//        let container = try decoder.container(keyedBy: CodingKeys.self)
-//        self.name = Dynamic(try container.decode(String.self, forKey: .name))
-//        self.currentTemperature = try container.decode(TemperatureViewModel.self, forKey: .currentTemperature)
-//    }
-//
-//    private enum CodingKeys: String, CodingKey {
-//        case name
-//        case currentTemperature = "main"
-//    } 
-//
-//}
-
 struct UserListVM: TableViewModel {
     
     
     var tableTitle: String
     var user: User?
-    var usersVM: [UserVM] // { }
-//    weak var view:
+    var usersVM: [UserVM]  
     fileprivate var selectedIndexPath: IndexPath?
-    
+    var numberOfSections: Int {
+        return 1
+    }
+ 
+ 
+    // MARK: - INIT
+ 
     init() {
         self.usersVM = [UserVM]()
     }
     
-//    private var weatherViewModels = [WeatherViewModel]()
-//
-//    mutating func addWeatherViewModel(_ vm: WeatherViewModel) {
-//        self.weatherViewModels.append(vm)
-//    }
     
-    var numberOfSections: Int {
-        return 1
-    }
-    
+     // MARK: - ACTIONS
+ 
     func numberOfRowsInSection(_ section: Int) -> Int {
         self.usersVM.count
     }
-    
+ 
     func userAt(_ indexPath: Int) -> UserVM {
         return self.usersVM[indexPath]
     }
     
-//    func viewModelForSelectedRow() -> UserDetailsVM? { // called in prepareForSegue:
-//    // builds vm for detailVC
-//        guard let selectedIndexPath = selectedIndexPath else {
-//            return nil
-//        }
-//        return UserDetailsVM(view: nil, model: usersVM[selectedIndexPath.row])
-//    }
+   func viewModelForSelectedRow() -> UserDetailsVM? { // called in prepareForSegue:
+      guard let selectedIndexPath = selectedIndexPath else {
+              return nil
+      }
+      return UserDetailsVM(view: nil, model: usersVM[selectedIndexPath.row])
+    }
     
     func setupSearchBarPlaceholderTextColor() {
         UITextField.appearance(whenContainedInInstancesOf: [UISearchBar.self]).defaultTextAttributes = [NSAttributedString.Key.foregroundColor: UIColor.darkGray]
@@ -80,12 +59,7 @@ struct UserListVM: TableViewModel {
     
     func cellViewModel(forIndexPath indexPath: IndexPath) -> CellViewModel? {  // called in cellForRow
         // builds vm for cell
-//            let row = indexPath.row
-//            if row < 0 || row >= self.coulorData.count {
-//                return nil
-//            }
-//            let cellText = coulorData[row].name
-            return TableViewCellViewModel(view: nil, cellText: "hello")
+        return TableViewCellViewModel(view: nil, cellText: "hello")
     }
     
     mutating func selectRow(atIndexPath indexPath: IndexPath) {
@@ -101,19 +75,9 @@ struct UserListVM: TableViewModel {
 //        return coulorData[row] as AnyObject
     }
     
-    func viewModelForSelectedRow() -> UserDetailsVM? {  // called in prepareForSegue:
-        //    // builds vm for detailVC
-        //        guard let selectedIndexPath = selectedIndexPath else {
-        //            return nil
-        //        }
-        //        return UserDetailsVM(view: nil, model: usersVM[selectedIndexPath.row])
-    }
-    
     func viewDidAppear(_ animated: Bool) {
-        <#code#>
     }
     
-    func setView(delegate: AnyObject?) {
-//        self.view = delegate as? ColorListTableViewControllerDelegate
+    func setView(delegate: AnyObject?) { 
     }
 }
